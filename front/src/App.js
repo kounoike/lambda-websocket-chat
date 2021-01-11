de13d7eb5bd844811a1e53f37e85ef2be01ed0c6
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2021-01-11 23:37:05 +0900

    ゴリっと書き換え

--- front/src/App.js
+++ front/src/App.js
@@ -1,37 +1,62 @@
-import React, { useState, useRef } from 'react';
-import { usePdf } from '@mikecousins/react-pdf';
+import React, { useEffect, useState, useRef } from 'react';
+import {usePdf} from '@mikecousins/react-pdf';
 
 const App = () => {
-  const [page, setPage] = useState(1);
   const canvasRef = useRef(null);
+  const [page, setPage] = useState(1);
 
-  const { pdfDocument } = usePdf({
+  const { pdfDocument, pdfPage } = usePdf({
     file: 'test.pdf',
     page,
     canvasRef,
   });
 
+  useEffect(() => {
+    if(pdfDocument){
+      const socket = new WebSocket("ws://localhost:5001/")
+      socket.onopen = (event) => {
+        // クライアント接続時
+        console.log("onopen", event);
+      };
+  
+      socket.onmessage = (event) => {
+        // サーバーからのメッセージ受信時
+        console.log("onmessgae", event);
+        console.log("pdfDocument", pdfDocument);
+        if (event.data.startsWith("next") && page !== pdfDocument.numPages){
+          setPage(page + 1);
+        }
+        else if (event.data.startsWith("prev") && page !== 1){
+          setPage(page - 1);
+        }
+      };
+  
+      socket.onclose = (event) => {
+        // クライアント切断時
+        console.log("onclose", event);
+      };
+      return () => {
+        socket.close();
+      }
+    }
+  }, [pdfDocument, page]);
+
   return (
     <div>
-      {!pdfDocument && <span>Loading...</span>}
+{!pdfDocument && <span>Loading...</span>}
       <canvas ref={canvasRef} style={{border: "1px solid blue"}} />
       {Boolean(pdfDocument && pdfDocument.numPages) && (
         <nav>
-          <ul className="pager">
-            <li className="previous">
-              <button disabled={page === 1} onClick={() => setPage(page - 1)}>
-                Previous
-              </button>
-            </li>
-            <li className="next">
-              <button
-                disabled={page === pdfDocument.numPages}
-                onClick={() => setPage(page + 1)}
-              >
-                Next
-              </button>
-            </li>
-          </ul>
+          <button disabled={page === 1} onClick={() => setPage(page - 1)}>
+            Previous
+          </button>
+          <button
+            disabled={page === pdfDocument.numPages}
+            onClick={() => setPage(page + 1)}
+          >
+            Next
+          </button>
+          <span>page: {page}</span>
         </nav>
       )}
     </div>
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ var server = require('ws').Server;
 var s = new server({port:5001});
 
 s.on('connection',function(ws){
-
+    console.log('I got a client');
     ws.on('message',function(message){
         console.log("Received: "+message);
 
