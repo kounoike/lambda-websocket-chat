d6bfa67dd6e8effd1dc125e459e1d436d11214e5
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2021-01-11 22:30:52 +0900

    cdk上手く行かない！！！

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -2,6 +2,7 @@ from aws_cdk import core
 import aws_cdk.aws_dynamodb as dynamodb
 import aws_cdk.aws_apigatewayv2 as apigwv2
 from aws_cdk.aws_lambda_python import PythonFunction
+import aws_cdk.aws_iam as iam
 
 
 class CdkStack(core.Stack):
@@ -17,6 +18,7 @@ class CdkStack(core.Stack):
             ),
             table_name="WebSocketConnection",
             billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+            removal_policy=core.RemovalPolicy.DESTROY,
         )
         api = apigwv2.CfnApi(
             self,
@@ -51,15 +53,33 @@ class CdkStack(core.Stack):
         ddb_table.grant_write_data(connect_lambda)
         ddb_table.grant_write_data(disconnect_lambda)
         ddb_table.grant_read_write_data(message_lambda)
-        
+
         region = "ap-northeast-1"
+
+        role = iam.Role(
+            self,
+            "lambda-websocket-chat-role",
+            assumed_by=iam.ServicePrincipal("apigateway.amazonaws.com"),
+        )
+        role.add_to_policy(
+            iam.PolicyStatement(
+                effect=iam.Effect.ALLOW,
+                resources=[
+                    connect_lambda.function_arn,
+                    disconnect_lambda.function_arn,
+                    message_lambda.function_arn,
+                ],
+                actions=["lambda:InvokeFunction"],
+            )
+        )
 
         connect_integration = apigwv2.CfnIntegration(
             self,
             "ConnectLambdaIntegration",
             api_id=api.ref,
             integration_type="AWS_PROXY",
-            integration_uri=f"arn:aws:apigateway:{region}:lambda:path/2015-03-31/functions/{connect_lambda.function_arn}/invocations"
+            integration_uri=f"arn:aws:apigateway:{region}:lambda:path/2015-03-31/functions/{connect_lambda.function_arn}/invocations",
+            credentials_arn=role.role_arn,
         )
         connect_route = apigwv2.CfnRoute(
             self,
@@ -67,7 +87,7 @@ class CdkStack(core.Stack):
             api_id=api.ref,
             route_key="$connect",
             authorization_type="NONE",
-            target=f"integrations/{connect_integration.ref}"
+            target=f"integrations/{connect_integration.ref}",
         )
 
         disconnect_integration = apigwv2.CfnIntegration(
@@ -75,7 +95,8 @@ class CdkStack(core.Stack):
             "DisconnectLambdaIntegration",
             api_id=api.ref,
             integration_type="AWS_PROXY",
-            integration_uri=f"arn:aws:apigateway:{region}:lambda:path/2015-03-31/functions/{disconnect_lambda.function_arn}/invocations"
+            integration_uri=f"arn:aws:apigateway:{region}:lambda:path/2015-03-31/functions/{disconnect_lambda.function_arn}/invocations",
+            credentials_arn=role.role_arn,
         )
         disconnect_route = apigwv2.CfnRoute(
             self,
@@ -83,7 +104,7 @@ class CdkStack(core.Stack):
             api_id=api.ref,
             route_key="$disconnect",
             authorization_type="NONE",
-            target=f"integrations/{disconnect_integration.ref}"
+            target=f"integrations/{disconnect_integration.ref}",
         )
 
         message_integration = apigwv2.CfnIntegration(
@@ -91,13 +112,25 @@ class CdkStack(core.Stack):
             "MessageLambdaIntegration",
             api_id=api.ref,
             integration_type="AWS_PROXY",
-            integration_uri=f"arn:aws:apigateway:{region}:lambda:path/2015-03-31/functions/{message_lambda.function_arn}/invocations"
+            integration_uri=f"arn:aws:apigateway:{region}:lambda:path/2015-03-31/functions/{message_lambda.function_arn}/invocations",
+            credentials_arn=role.role_arn,
         )
         message_route = apigwv2.CfnRoute(
             self,
             "MessageLambdaRoute",
             api_id=api.ref,
-            route_key="$message",
+            route_key="sendmessage",
             authorization_type="NONE",
-            target=f"integrations/{message_integration.ref}"
+            target=f"integrations/{message_integration.ref}",
         )
+
+        deployment = apigwv2.CfnDeployment(self, "lambda-websocket-chat-deployment", api_id=api.ref)
+
+        stage = apigwv2.CfnStage(self, "lambda-websocket-chat-stage", api_id=api.ref, auto_deploy=True, deployment_id=deployment.ref, stage_name="dev")
+
+        deps = core.ConcreteDependable()
+        deps.add(connect_route)
+        deps.add(disconnect_route)
+        deps.add(message_route)
+        deployment.node.add_dependency(deps)
+        
--- lambda/app.py
+++ lambda/app.py
@@ -1,35 +1,39 @@
 import boto3
 
-dynamodb = boto3.resource('dynamodb')
-connections = dynamodb.Table(os.environ['WebSocketConnection'])
+dynamodb = boto3.resource("dynamodb")
+connections = dynamodb.Table(os.environ["WebSocketConnection"])
+
 
 def connect_handler(event, context):
-    connection_id = event.get('requestContext',{}).get('connectionId')
-    result = connections.put_item(Item={ 'connectionId': connection_id })
-    return { 'statusCode': 200,'body': 'ok' }
+    connection_id = event.get("requestContext", {}).get("connectionId")
+    result = connections.put_item(Item={"connectionId": connection_id})
+    return {"statusCode": 200, "body": "ok"}
+
 
 def disconnect_handler(event, context):
-    connection_id = event.get('requestContext',{}).get('connectionId')
-    result = connections.delete_item(Key={ 'connectionId': connection_id })
-    return { 'statusCode': 200, 'body': 'ok' }
+    connection_id = event.get("requestContext", {}).get("connectionId")
+    result = connections.delete_item(Key={"connectionId": connection_id})
+    return {"statusCode": 200, "body": "ok"}
 
 def message_handler(event, context):
-    post_data = json.loads(event.get('body', '{}')).get('data')
+    post_data = json.loads(event.get("body", "{}")).get("data")
     # print(post_data)
-    domain_name = event.get('requestContext',{}).get('domainName')
-    stage       = event.get('requestContext',{}).get('stage')
+    domain_name = event.get("requestContext", {}).get("domainName")
+    stage = event.get("requestContext", {}).get("stage")
 
-    items = connections.scan(ProjectionExpression='connectionId').get('Items')
+    items = connections.scan(ProjectionExpression="connectionId").get("Items")
     if items is None:
-        return { 'statusCode': 500,'body': 'something went wrong' }
+        return {"statusCode": 500, "body": "something went wrong"}
 
-    apigw_management = boto3.client('apigatewaymanagementapi',
-                                    endpoint_url=F"https://{domain_name}/{stage}")
+    apigw_management = boto3.client(
+        "apigatewaymanagementapi", endpoint_url=f"https://{domain_name}/{stage}"
+    )
     for item in items:
         try:
             print(item)
-            _ = apigw_management.post_to_connection(ConnectionId=item['connectionId'],
-                                                         Data=post_data)
+            _ = apigw_management.post_to_connection(
+                ConnectionId=item["connectionId"], Data=post_data
+            )
         except:
             pass
-    return { 'statusCode': 200,'body': 'ok' }
+    return {"statusCode": 200, "body": "ok"}
